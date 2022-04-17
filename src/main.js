3318de74cb017079197482cac28c6e6cf1419b12
migodau <migodau@gmail.com>
2022-04-16 21:13:15 -0300

    config pinia and defining store

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/base.css'
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(createPinia())
 
 app.mount('#app')
